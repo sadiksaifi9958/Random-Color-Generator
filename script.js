95e67354b2da374b769d2607b2db40b1c2f8c92a
Sadik Saifi <sadiksaifi7428@gmail.com>
2026-04-14 12:34:46 +0530

    add hex conversion and copy to clipboard feature

--- script.js
+++ script.js
@@ -1,21 +1,62 @@
+
 let btn = document.querySelector("button");
 let colorValue = document.querySelector(".color-value");
+let hexCode = document.querySelector(".hex-code");
+let box = document.querySelector("div");
+
+
+let currentRgb = "";
+let currentHex = "";
 
 
 btn.addEventListener("click", function () {
-    let heading = document.querySelector("h1");
-    let randomColor = getRandomColor();
-    colorValue.innerText = randomColor;
-    let box = document.querySelector("div");
-    box.style.background = randomColor;
+
+    let colors = getRandomColor();
+    currentRgb = colors.rgb;
+    currentHex = colors.hex;
+
+
+    colorValue.innerHTML = `${currentRgb} <i class="fa-regular fa-clipboard"></i>`;
+    colorValue.style.backgroundColor = currentRgb;
+
+
+    hexCode.innerHTML = `${currentHex} <i class="fa-regular fa-clipboard"></i>`;
+    hexCode.style.backgroundColor = currentHex;
+
+
+    box.style.background = currentRgb;
 });
+
+
+colorValue.addEventListener("click", function (e) {
+    if (e.target.classList.contains("fa-clipboard")) {
+        navigator.clipboard.writeText(currentRgb);
+        colorValue.innerHTML = `${currentRgb} <i class="fa-solid fa-check"></i>`;
+        setTimeout(function () {
+            colorValue.innerHTML = `${currentRgb} <i class="fa-regular fa-clipboard"></i>`;
+        }, 2000);
+    }
+});
+
+
+hexCode.addEventListener("click", function (e) {
+    if (e.target.classList.contains("fa-clipboard")) {
+        navigator.clipboard.writeText(currentHex);
+        hexCode.innerHTML = `${currentHex} <i class="fa-solid fa-check"></i>`;
+        setTimeout(function () {
+            hexCode.innerHTML = `${currentHex} <i class="fa-regular fa-clipboard"></i>`;
+        }, 2000);
+    }
+});
+
 
 function getRandomColor() {
     let red = Math.floor(Math.random() * 256);
     let green = Math.floor(Math.random() * 256);
     let blue = Math.floor(Math.random() * 256);
 
+    let rgb = `rgb(${red}, ${green}, ${blue})`;
+    let hex = `#${red.toString(16).padStart(2, "0")}${green.toString(16).padStart(2, "0")}${blue.toString(16).padStart(2, "0")}`.toUpperCase();
 
-    let color = `rgb(${red}, ${green}, ${blue} )`;
-    return color;
-}   
+    return { rgb, hex };
+}
